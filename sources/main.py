b8c7b933bd257377276d415702e612db4ec1b53e
klmurussi <klmurussi@hotmail.com>
2021-02-20 17:58:34 -0300

    Corrigindo o número de jogadores

--- sources/main.py
+++ sources/main.py
@@ -98,21 +98,20 @@ class Game:
 # create the game object
 g = Game()
 g.new()
-if len(sys.argv) <= 2 or len(sys.argv) > 7:
+if len(sys.argv) <= 3 or len(sys.argv) > 7:
     print ("Número inválido de jogadores.")
     g.quit()
 
 g.start_game()
 while True:
     g.arrange()
-    if len(sys.argv) >= 3:
+    if len(sys.argv) >= 4:
         g.play1(sys.argv[1])
         g.play2(sys.argv[2])
-        if len(sys.argv) >= 4:
-            g.play3(sys.argv[3])
-            if len(sys.argv) >= 5:
-                g.play4(sys.argv[4])
-                if len(sys.argv) >= 6:
-                    g.play5(sys.argv[5])
-                    if len(sys.argv) == 7:
-                        g.play6(sys.argv[6])
+        g.play3(sys.argv[3])
+        if len(sys.argv) >= 5:
+            g.play4(sys.argv[4])
+            if len(sys.argv) >= 6:
+                g.play5(sys.argv[5])
+                if len(sys.argv) == 7:
+                    g.play6(sys.argv[6])
